432081bbdcc69309df5b59f3cf298965ed8af1b6
Jory Roberts <robertsjory13@gmail.com>
2023-04-18 08:42:44 -0500

    fetch add card

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+/*
+Deliverables:
+1. Add toy info to the card
+2. add a new toy POST request
+3.increase toy's likes
+
+*/
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,3 +21,50 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const addBtn = document.querySelector("#new-toy-btn");
+console.log(addBtn);
+const toyForm = document.querySelector(".container");
+console.log(toyForm);
+
+getToy = () => {
+  return fetch("http://localhost:3000/toys")
+    .then((res) => res.json())
+    .then((data) => {
+      let toyCollection = document.querySelector("#toy-collection");
+      console.log(toyCollection);
+
+      data.forEach((toy) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const name = document.createElement("h2");
+        name.textContent = toy.name;
+
+        const image = document.createElement("img");
+        image.src = toy.image;
+        image.classList.add("toy-avatar");
+
+        const likes = document.createElement("p");
+        likes.textContent = `Likes: ${toy.likes}`;
+
+        const likeBtn = document.createElement("button");
+        likeBtn.classList.add("like-btn");
+        likeBtn.id = toy.id;
+        likeBtn.textContent = "Like";
+
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(likes);
+        card.appendChild(likeBtn);
+
+        toyCollection.appendChild(card);
+      });
+    });
+};
+
+console.log(getToy());
+
+toyLikes = (toy) => {};
+
+createToys = (toy) => {};
